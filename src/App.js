3da5f213d32aefc6490902353820576d9e98ade4
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-14 18:54:51 +0500

    template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <div className='main-div'> 
+        <div className='input-field'>
+          <input  className="input-todo" type='text' placeholder='Add Your ToDos' id='todos' name='todos'/>
+          <button className='add-btn'>ADD</button>
+        </div>
+       </div>
+       
+      </div>
+    </>
   );
 }
 
