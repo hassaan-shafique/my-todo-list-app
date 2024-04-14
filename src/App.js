d494b0bf00874566a7e13f3c3d7ecb48683d27db
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-14 19:02:30 +0500

    template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, SetTodo] =useState("");
+
+ 
   return (
     <>
       <div className="App">
       <div className='main-div'> 
         <div className='input-field'>
           <input  className="input-todo" type='text' placeholder='Add Your ToDos' id='todos' name='todos'/>
-          <button className='add-btn'>ADD</button>
+          <button className='add-btn' >ADD</button>
         </div>
        </div>
        
