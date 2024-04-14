ff0a17aeab21f078c254172cab30c755f97c81cc
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-14 20:52:54 +0500

    todos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todo, SetTodo] =useState("");
+  const [todo, SetTodo] =useState([]);
 
  
   return (
@@ -11,11 +11,24 @@ function App() {
       <div className="App">
       <div className='main-div'> 
         <div className='input-field'>
-          <input  className="input-todo" type='text' placeholder='Add Your ToDos' id='todos' name='todos'/>
+          <input  className="input-todo" type='text'
+          value= {todo}
+        placeholder='Add Your ToDos' id='todos' name='todos'/>
           <button className='add-btn' >ADD</button>
         </div>
-       </div>
-       
+        <h3> Your ToDos </h3>
+        <div className='todos'>
+        <div> <h4>this is a task 1</h4> </div>
+        <div> 
+        <button className='add-btn'>
+Edit
+        </button>
+        <button className='add-btn'> Delete</button></div>
+          </div>
+           </div>
+
+        
+      
       </div>
     </>
   );
