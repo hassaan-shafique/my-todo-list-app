151d0aae6bca9b510a6cf869176148c9db3dd9bc
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-20 23:03:01 +0500

    add tasks

--- src/App.js
+++ src/App.js
@@ -3,32 +3,80 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todo, SetTodo] =useState([]);
+  
+  const[inputVal,setInputVal] =useState("");
+  const [todo, SetTodo] = useState([]);
 
+  const HandleEdit =()=> {
+
+  }
+
+  const HandleDelete =()=> {
+    
+  }
+
+  const HandleAdd = () => {
+  
+   SetTodo([...todo,inputVal]);
+   setInputVal('');
+   
+  }
+  // const HandleChange = (e) => {
+    
+    
+  //   setInputVal(e.target.value);
+
+  // }
  
   return (
     <>
       <div className="App">
-      <div className='main-div'> 
-        <div className='input-field'>
-          <input  className="input-todo" type='text'
-          value= {todo}
-        placeholder='Add Your ToDos' id='todos' name='todos'/>
-          <button className='add-btn' >ADD</button>
-        </div>
-        <h3> Your ToDos </h3>
-        <div className='todos'>
-        <div> <h4>this is a task 1</h4> </div>
-        <div> 
-        <button className='add-btn'>
-Edit
-        </button>
-        <button className='add-btn'> Delete</button></div>
+        <h1 className="head"> To-Do List </h1>
+        <div className="main-div">
+          <div className="input-field">
+            <input
+              className="input-todo"
+              onChange={(e) => setInputVal(e.target.value)}
+              value={inputVal}
+              placeholder="Add Your ToDos"
+              id="todos"
+              name="todos"
+            />
+            <button className="add-btn" onClick={HandleAdd}>
+              ADD
+            </button>
           </div>
-           </div>
+          <div>
+            <h2>Your Todos:</h2>
 
-        
-      
+            <div className="todos">
+              <div className="tasks">
+                
+                <div>
+                  <ul>
+                    {todo.map((todo, index) => {
+                      return (
+                        <li key={index}>
+                          <input type="checkbox" />
+                          {todo}
+                          <button className="add-btn" onClick={HandleEdit}>
+                            Edit
+                          </button>
+                          <button className="add-btn" onClick={HandleDelete}>
+                            Delete
+                          </button>
+                        </li>
+                      );
+                    })}
+
+                    
+                  </ul>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
